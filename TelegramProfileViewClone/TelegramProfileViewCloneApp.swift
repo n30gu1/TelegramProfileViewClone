3ede6d4eabd2fc01866a41499cfd748a1df3fddb
n30gu1 <n@n30gu1.com>
2022-08-30 21:42:40 +0900

    Initial Commit

---
+++ TelegramProfileViewClone/TelegramProfileViewCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramProfileViewCloneApp.swift
+//  TelegramProfileViewClone
+//
+//  Created by Sung Park on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct TelegramProfileViewCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
